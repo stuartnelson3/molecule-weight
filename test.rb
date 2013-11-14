d1a02880c85c5b6fcaba3c678ec7635d5d5beae6
Stuart Nelson <stuartnelson3@gmail.com>
2013-11-14 09:58:08 -0500

    change 3_ pattern to (3_), change regex to scan for that, and fix tests based on this change

--- test.rb
+++ test.rb
@@ -2,23 +2,25 @@ require 'rspec'
 require './main'
 
 describe UserInput do
-  let(:parser) { UserInput }
+  let(:parser)               { UserInput }
+  let(:expected_parsed_data) { %w(a (3l) (3t) x z) }
+
   it "splits on the user input" do
     expect(parser.parse("XZU")).to eq(%w(x z u))
   end
 
   it "splits with the number inputs" do
-    expect(parser.parse("A3L3TXZ")).to eq(%w(a 3l 3t x z))
+    expect(parser.parse("A(3L)(3T)XZ")).to eq(expected_parsed_data)
   end
 
   it "handles spaces in the input" do
-    expect(parser.parse("A 3L 3T X Z")).to eq(%w(a 3l 3t x z))
+    expect(parser.parse("A (3L) (3T) X Z")).to eq(expected_parsed_data)
   end
 
 end
 
 describe AminoAcid do
-  let(:subject) { AminoAcid.new("AEF3AYXZ") }
+  let(:subject) { AminoAcid.new("AEF(3A)YXZ") }
 
   it "calculates the molecule weight" do
     expect(subject.weight).to be_within(0.1).of(818.39 + 19)
@@ -27,15 +29,15 @@ describe AminoAcid do
   it "knows the possible combinations of the molecules" do
     expect(subject.combinations).to eq(
       [
-        ["a"], ["e"], ["f"], ["3a"], ["y"], ["x"], ["z"],
-        ["a", "e"], ["e", "f"], ["f", "3a"], ["3a", "y"],
-        ["y", "x"], ["x", "z"], ["a", "e", "f"], ["e", "f", "3a"],
-        ["f", "3a", "y"], ["3a", "y", "x"], ["y", "x", "z"],
-        ["a", "e", "f", "3a"], ["e", "f", "3a", "y"],
-        ["f", "3a", "y", "x"], ["3a", "y", "x", "z"],
-        ["a", "e", "f", "3a", "y"], ["e", "f", "3a", "y", "x"],
-        ["f", "3a", "y", "x", "z"], ["a", "e", "f", "3a", "y", "x"],
-        ["e", "f", "3a", "y", "x", "z"], ["a", "e", "f", "3a", "y", "x", "z"]
+        ["a"], ["e"], ["f"], ["(3a)"], ["y"], ["x"], ["z"],
+        ["a", "e"], ["e", "f"], ["f", "(3a)"], ["(3a)", "y"],
+        ["y", "x"], ["x", "z"], ["a", "e", "f"], ["e", "f", "(3a)"],
+        ["f", "(3a)", "y"], ["(3a)", "y", "x"], ["y", "x", "z"],
+        ["a", "e", "f", "(3a)"], ["e", "f", "(3a)", "y"],
+        ["f", "(3a)", "y", "x"], ["(3a)", "y", "x", "z"],
+        ["a", "e", "f", "(3a)", "y"], ["e", "f", "(3a)", "y", "x"],
+        ["f", "(3a)", "y", "x", "z"], ["a", "e", "f", "(3a)", "y", "x"],
+        ["e", "f", "(3a)", "y", "x", "z"], ["a", "e", "f", "(3a)", "y", "x", "z"]
       ]
     )
   end
