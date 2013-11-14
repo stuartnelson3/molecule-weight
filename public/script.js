ea7e2bb6b8965d29b09e1dac5f535db133e69f62
Stuart Nelson <stuartnelson3@gmail.com>
2013-11-14 17:11:39 -0500

    clear weight on each request

--- public/script.js
+++ public/script.js
@@ -20,7 +20,7 @@ function postData(e) {
       data = JSON.parse(data)
       seq_objs = data['possible_sequences'];
 
-      $(".peptide-weight").append("<p>Peptide weight is "+data['weight']+"</p>");
+      $(".peptide-weight").empty().append("<p>Peptide weight is "+data['weight']+"</p>");
       if (typeof seq_objs === "string") {
         $(".result table").append("<tr><td>"+seq_objs+"</td></tr>")
         return
