a10988133ef764b120ef6f85ac61cbdd22b661b1
Stuart Nelson <stuartnelson3@gmail.com>
2013-11-15 10:01:08 -0500

    fixed some mistaken global vars in script.js

--- public/script.js
+++ public/script.js
@@ -11,25 +11,25 @@ function postData(e) {
     weight: parseFloat($("input[name=weight]").val())
   };
 
-  params = {
+  var params = {
     type: 'POST',
     data: data,
     url: '/possible-matches',
     success: function(data) {
-      data = JSON.parse(data)
-      seq_objs = data['possible_sequences'];
+      data = JSON.parse(data);
+      var seq_objs = data['possible_sequences'];
 
       $(".peptide-weight").empty().append("<p>Peptide weight is "+data['weight']+"</p>");
       if (typeof seq_objs === "string") {
-        $(".result table").append("<tr><td class='error'>"+seq_objs+"</td></tr>")
-        return
+        $(".result table").append("<tr><td class='error'>"+seq_objs+"</td></tr>");
+        return;
       }
 
       var sequences = Object.keys(seq_objs);
       for (var i = 0; i < sequences.length; i++) {
-        var sequence = sequences[i]
-        var weight = seq_objs[sequence]
-        $(".result table").append("<tr><td>"+sequence+"</td><td>"+weight+"</td></tr>")
+        var sequence = sequences[i];
+        var weight = seq_objs[sequence];
+        $(".result table").append("<tr><td>"+sequence+"</td><td>"+weight+"</td></tr>");
       }
     }
   }
