c3d6194c85fda21f7d5271aa96e5f13f26b00f94
stuart nelson <stuartnelson3@gmail.com>
2017-04-07 13:49:42 +0200

    Check for peptide sequence and found weight

--- web.rb
+++ web.rb
@@ -8,11 +8,18 @@ end
 
 post '/possible-matches' do
   begin
+    if params[:peptide_sequence] == ""
+      return { 'possible_sequences' => 'missing peptide sequence' }.to_json
+    end
+    if params[:weight] == "NaN"
+      return { 'possible_sequences' => 'missing weight' }.to_json
+    end
+
     mass_type = MassTypes.parse params[:mass_type]
     # TODO: Reject if no tolerance value given
     tolerance = params[:tolerance].to_f
     if tolerance < 5
-    { 'possible_sequences' => 'error: tolerance below 5 Da' }.to_json
+      { 'possible_sequences' => 'error: tolerance below 5 Da' }.to_json
     end
     residues = AminoAcids.new(mass_type).residues
     p = Peptide.new params[:peptide_sequence], residues, params[:wildcards] || {}
